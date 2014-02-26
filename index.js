57f49d59b382cd1d501122356907f28e235115a8
Marko Bregant <marko@bregant.si>
2014-02-26 22:04:00 +0100

    Remove revision log

--- index.js
+++ index.js
@@ -1,45 +1,7 @@
 // index.js
 
 "use strict";
-/*
-var revisionLog = {};
 
-function addLog(log) {
-    revisionLog[Date.now()] = log;
-}
-
-var readline = require('readline');
-
-var rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
-
-rl.setPrompt('What is on your mind? ');
-rl.prompt();
-
-rl.on('line', function(line) {
-
-    switch (line.trim()) {
-        case 'exit':
-            rl.close();
-            break;
-        case 'log':
-            console.log(revisionLog);
-            break;
-        default:
-            addLog(line);
-            break;
-    }
-    rl.prompt();
-
-}).on('close', function() {
-
-    console.log('Have a great day!');
-    process.exit(0);
-
-});
-*/
 function showTable(table, longerText, shorterText) {
     var space = "   ";
 
