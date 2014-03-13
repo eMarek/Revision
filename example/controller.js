4346a1ca201daa26811b30abe85a6b974d5637fb
Marko Bregant <marko@bregant.si>
2014-03-14 00:04:52 +0100

    Authorization checking in controller

--- example/controller.js
+++ example/controller.js
@@ -6,7 +6,19 @@ var r = require('rethinkdb');
 
 module.exports = function(request, response, data, handler) {
 
+    // example data
     data['goal'] = "Operational Transformation";
 
-    handler(request, response, data);
+    // checking authorization
+    if (request.headers.hasOwnProperty("authorization") && request.url != "/api/login.json") {
+
+        var authorization = request.headers.authorization;
+
+        response.send({
+            "say": "out"
+        });
+
+    } else {
+        handler(request, response, data);
+    }
 };
