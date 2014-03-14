3041d1fd5a0a97e843bd5472729663d2537c7e31
Marko Bregant <marko@bregant.si>
2014-03-14 02:15:18 +0100

    Authorization in controller

--- example/controller.js
+++ example/controller.js
@@ -9,16 +9,72 @@ module.exports = function(req, rsp, data, handler) {
     // example data
     data["thesis"] = "Operational Transformation";
 
-    // checking session
-    if (!req.headers.hasOwnProperty("session") && req.url != "/api/login.json") {
+    // public paths
+    var publicPaths = ["/api/login.json"];
 
-        rsp.send({
-            "say": "out"
-        });
-
-    } else {
-
-        var session = req.headers.session;
+    if (publicPaths.indexOf(req.url) >= 0) {
         handler(req, rsp, data);
+        return;
     }
+
+    // do i have session
+    if (!req.headers.hasOwnProperty("session")) {
+        rsp.send({
+            "say": "out",
+            "msg": "Za nadaljevanje se moraš prijaviti."
+        });
+        return;
+    }
+    var session = req.headers.session;
+
+    // decrypt session
+    var decipher = require("crypto").createDecipher("aes256", data.key);
+    var sessionString = decipher.update(session, "hex", "utf8") + decipher.final("utf8");
+    var sessionJSON = JSON.parse(sessionString);
+
+    if (session.length < 500 || !sessionJSON || typeof sessionJSON != "object") {
+        rsp.send({
+            "say": "out",
+            "msg": "Seja je pokvarjena."
+        });
+        return;
+    }
+
+    // check session data
+    if (!sessionJSON.userId || !sessionJSON.username || !sessionJSON.ip || !sessionJSON.userAgent || !sessionJSON.key || !sessionJSON.timeStamp) {
+        rsp.send({
+            "say": "out",
+            "msg": "Seja ne vsebuje vseh predvidenih podatkov."
+        });
+        return;
+    }
+
+    // check session adequacy
+    if (sessionJSON.ip != req.headers.host || sessionJSON.userAgent != req.headers['user-agent'] || sessionJSON.key != data.key) {
+        rsp.send({
+            "say": "out",
+            "msg": "Podatki seje ne ustrezajo predvidenim podatkom."
+        });
+        return;
+    }
+
+    r.db("revision").table("users").get(sessionJSON.userId).run(data.conn, function(err, result) {
+        if (err) throw err;
+
+        var user = result;
+
+        if (!user) {
+            rsp.send({
+                "say": "out",
+                "msg": "Uporabnik iz seje ne obstaja."
+            });
+            return;
+        }
+
+        // user data
+        data.user = user;
+
+        // handle request
+        handler(req, rsp, data);
+    });
 };
