191502a7d93596e6935c9c2410b66b61f10fbded
Marko Bregant <marko@bregant.si>
2014-03-29 12:51:41 +0100

    Init document

--- collaboration.js
+++ collaboration.js
@@ -2,10 +2,24 @@
 
 "use strict";
 
+var serverData = {
+    "waitingChanges": [],
+    "revisionDiary": [],
+    "currentDocument": ""
+};
+
 module.exports = function example(req, rsp, data) {
 
+    if (req.payload.operation === "init") {
+        rsp.send({
+            "say": "yay",
+            "currentDocument": serverData.currentDocument
+        });
+        return;
+    }
+
     rsp.send({
-        "say": "yay",
-        "collaboration": true
+        "say": "noo",
+        "msg": "Ne vem kaj narediti!"
     });
 };
